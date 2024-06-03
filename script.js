6d4251f9c358dc770015a9f09d46802412c9ee61
Kristen <pixilietv@gmail.com>
2024-06-03 21:15:03 +0200

    fedfEFD

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
+console.log('Script loaded');
+
 const button = document.querySelector('input[type="button"]');
 const input = document.querySelector('input[type="text"]');
 const number = document.querySelector('.essais');
 const result = document.querySelector('#valider .reponse_valide');
+const code_img = document.querySelector('.code');
+console.log(code_img)
 
 const correction = ["basion", "cadeau", "fuji", "ligneux"];
 
 let good_answers = 0;
-let counter = 0
+let counter = 0;
+let validé = []
+
+const code = `
+def dechiffrage_cesar_recursif(texte_chiffre, dec = 0):
+    if dec == 26:
+        return []
+    else :
+        texte = ""
+        for caractere in texte_chiffre:
+            lettre_dechiffree = (chr((ord(caractere)+ ... - ...)%26 +97))
+            texte +=(...)
+        return [texte] + ...
+
+
+texte_chiffre = "usvwsm"
+texte_dechiffre = dechiffrage_cesar_recursif(texte_chiffre)
+print("déchiffrages possibles :", texte_dechiffre`
+
+function copy() {
+    navigator.clipboard.writeText(code);
+    alert("Code copié dans le presse-papier");
+  } 
+
+code_img.addEventListener('click', () => {
+    copy()
+})
 
 button.addEventListener('click', () => {
-    const input_value = input.value
+    let input_value = input.value
 
     if (input_value === '') {
         alert('Veuillez entrer une réponse avant de valider')
@@ -17,17 +47,20 @@ button.addEventListener('click', () => {
         counter = counter + 1
         number.textContent = `Nombre d'essais: ${counter}`
         
-        if (correction.includes(input_value)) {
+        if (correction.includes(input_value) && !validé.includes(input_value)) {
             if (correction.includes(input_value)) {
                 good_answers = good_answers + 1
                 result.textContent = `${good_answers}/${correction.length} réponses valides`
+                validé.push(input_value)
+                input_value = "";
 
                 if (good_answers === correction.length) {
                     document.location.href = './fini/reussi.html'
                 }
             }
         } else {
-            alert('Mauvaise réponse ;)')
+            alert('Mauvaise réponse ou réponse déjà validée ;)')
+            input_value = "";
         }
     }
 })
